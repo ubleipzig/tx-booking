3236343a6919d1f323e27dc3ab83c42d57026b36
Ulf Seltmann <seltmann@ub.uni-leipzig.de>
2017-01-24 17:28:45 +0100

    refs #9598, #9599:  * removed unused code, made comments to further improvements  * fixed  duplicate booking race condition  * added distinction of own bookings in week view  * indentation  * sql schema changes

--- ext_tables.sql
+++ ext_tables.sql
@@ -37,7 +37,8 @@ CREATE TABLE tx_ubleipzigbooking (
 	memo varchar(255) DEFAULT '',
 
 	PRIMARY KEY (uid),
-	KEY parent (pid)
+	KEY parent (pid),
+	UNIQUE `unique_index`(`startdate`, `objectuid`)
 );
 
 
